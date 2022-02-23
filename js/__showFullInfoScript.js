ecadc621156fcfb18a00667e1af91b77b25e49e5
unknown <khuillebagares@gmail.com>
2022-02-23 11:07:19 +0800

    Added Webpage layout for president and vice president candidates and added hover info

---
+++ js/__showFullInfoScript.js
@@ -0,0 +1,12 @@
+var information = document.getElementById("information");
+var button = document.getElementById("show-info");
+
+button.onclick = function(){
+	if(information.className == "open"){
+		information.className = "";
+		
+	}
+	else{
+		information.className = "open";
+	}
+};
